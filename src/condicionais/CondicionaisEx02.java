408cdb5a3785c3dff4d191d06add3ad39ad3c625
julianadev <designsilvaport@yahoo.com>
2024-10-25 16:13:09 -0300

    condicionais com mais de duas possibilidades

---
+++ src/condicionais/CondicionaisEx02.java
@@ -0,0 +1,20 @@
+package condicionais;
+
+public class CondicionaisEx02 {
+    // mais de duas possibilidades
+
+    public static void main(String[] args) {
+        int hora = 20;
+
+        if (hora <=  12){
+            System.out.println("BOM DIA");
+
+        }else if (hora < 18){
+            System.out.
+                    println("BOA TARDE");
+        }else {
+            System.out.println("BOA NOITE");
+        }
+    }
+
+}
