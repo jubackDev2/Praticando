6fa4f180bc42d66be1955b5a69263e07021dd5f7
julianadev <designsilvaport@yahoo.com>
2024-10-25 15:03:10 -0300

    praticando e fazendo alguns exercicios

---
+++ src/condicionais/CondicionaisEx01.java
@@ -0,0 +1,28 @@
+package condicionais;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class CondicionaisEx01 {
+    public static void main(String[] args) {
+
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+
+
+        double nota1 = sc.nextDouble();
+        double nota2 = sc.nextDouble();
+
+        double notaFinal = nota1+ nota2;
+
+        System.out.printf("NOTA FINAL = %.1f%n", notaFinal);
+
+
+
+
+
+
+    }
+}
